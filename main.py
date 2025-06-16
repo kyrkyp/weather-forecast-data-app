2215b462aa397c2c2da0fe0b0a891cf450e93201
Kyriakos Kypraios <kkypraios@allweb.gr>
2025-06-16 20:17:50 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.title("Weather forecast for the next days")
+place = st.text_input("Enter a place to get the weather forecast: ")
+days = st.slider(
+    "Select the number of days for the forecast:",
+    min_value=1,
+    max_value=5,
+    help="Select the number of forecasted days")
+option = st.selectbox("Select data to view", ("Temperature", "Sky"))
+st.subheader(f"{option} forecast for {place} for the next {days} days")
