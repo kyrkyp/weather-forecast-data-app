5755ef54163d3e5abb6adc9391a56921c04d1673
Kyriakos Kypraios <kkypraios@allweb.gr>
2025-06-16 21:02:15 +0300

    Add backend implementation for external api call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
 
 st.title("Weather forecast for the next days")
@@ -10,3 +12,10 @@ days = st.slider(
     help="Select the number of forecasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} forecast for {place} for the next {days} days")
+
+data = get_data(place, days, option)
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (°C)"})
+st.plotly_chart(figure)
